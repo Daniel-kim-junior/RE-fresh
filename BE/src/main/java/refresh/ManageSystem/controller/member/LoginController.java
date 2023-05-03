648ef05a428738057db0cb613f17ee4dd9b0b1f4
jiokim-7 <now20412041@gmail.com>
2023-05-03 10:26:30 +0900

    fix: 세션 연결 Hot fix

--- BE/src/main/java/refresh/ManageSystem/controller/member/LoginController.java
+++ BE/src/main/java/refresh/ManageSystem/controller/member/LoginController.java
@@ -1,9 +1,6 @@
 package refresh.ManageSystem.controller.member;
 
 import java.util.Optional;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
