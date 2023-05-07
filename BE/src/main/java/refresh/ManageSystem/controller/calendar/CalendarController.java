26d5b038c190342349264d87791116c6bb7da36d
jiokim-7 <now20412041@gmail.com>
2023-05-07 19:18:34 +0900

    fix: /calendar -> /cal

--- BE/src/main/java/refresh/ManageSystem/controller/calendar/CalendarController.java
+++ BE/src/main/java/refresh/ManageSystem/controller/calendar/CalendarController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CalendarController {
 
-    @GetMapping("/calendar")
+    @GetMapping("/cal")
     public String get() {
         return "/pages/calendar/calendar";
     }
